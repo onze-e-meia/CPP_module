1a41925a6d382506baa780731d4e5660bec8bec4
onze-e-meia <tffuente@gmail.com>
2025-03-11 16:29:17 -0300

    Just a mess

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tforster <tfforster@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:16:48 by tforster          #+#    #+#             */
-/*   Updated: 2025/02/25 19:00:52 by tforster         ###   ########.fr       */
+/*   up_orderd: 2025/03/11 15:18:45 by tforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,58 @@
 #include <cmath>
 #include "lib/color.hpp"
 
-void	merge(std::vector<int> &vec);
-void	printMerge(std::size_t level, std::vector<int> &vec, bool before = true);
+// void	merge(std::vector<int> &vec);
+void	merge(int *input);
+// void	printMerge(std::size_t level, std::vector<int> &vec, bool before, bool merge);
+void	printMerge(std::size_t level, int *vec, bool before, bool merge);
+
+struct ctrl {
+	std::size_t _level;
+	std::size_t _vecSize;
+	std::size_t _order;
+	std::size_t _pairStart;
+	std::size_t _pairSize;
+	std::size_t _nb_pairs;
+	std::size_t _nbElements_completePs;
+	std::size_t _leftOverElements;
+	std::size_t _nbElements_noLeftOver;
+
+	// void up_order(std::size_t level, const std::vector<int> &vec) {
+	void up_order(std::size_t level) {
+		_level = level;
+		// _vecSize = vec.size();
+		_order = 1ull << level;
+		_pairStart = _order - 1;
+		_pairSize = 2 * _order;
+		_nb_pairs = _vecSize / _pairSize;
+		_nbElements_completePs = _nb_pairs * _pairSize;
+		_leftOverElements = (_vecSize - _nbElements_completePs) % _order;
+		_nbElements_noLeftOver = _vecSize - _leftOverElements;
+	}
+
+	// void down_order(std::size_t level, const std::vector<int> &vec) {
+	void down_order(std::size_t level) {
+		_level = level;
+		// _vecSize = vec.size();
+		_order >>= 1;
+		_pairStart = _order - 1;
+		_pairSize = 2 * _order;
+		_nb_pairs = _vecSize / _pairSize;
+		_nbElements_completePs = _nb_pairs * _pairSize;
+		_leftOverElements = (_vecSize - _nbElements_completePs) % _order;
+		_nbElements_noLeftOver = _vecSize - _leftOverElements;
+	}
+};
+
+void	print_status(ctrl *c) {
+	std::cout
+		<< " ORDER: " << c->_order << " PAIR_START: " << c->_pairStart << ENDL
+		<< " PAIR_SIZE: " << c->_pairSize << " NB_OF_PAIRS: " << c->_nb_pairs << ENDL
+		<< " NB_ELEMENTS_OF_COMPLETE_PAIRS: " << c->_nbElements_completePs << ENDL
+		<< " LEFTOVER_ELEMENTS: " << c->_leftOverElements % c->_order << ENDL
+		<< " NB_ELEMENTS_WITH_NO LEFTOVER :" << c->_nbElements_noLeftOver << ENDL;
+}
+
 
 int	main(int argc, char ** argv) {
 	std::system("clear");
@@ -54,153 +104,187 @@ int	main(int argc, char ** argv) {
 	for (std::size_t i = 0; i < size; ++i) {std::cout << vector[i] << " | ";}
 	std::cout << ENDL << ENDL;
 
-	std::cout << "FIRST MERGE: " ENDL;
-	merge(vector);
+	// merge(vector);
+	merge(input);
 	std::cout << ENDL;
-
-
-
-	// std::vector<int>::iterator it = vector.begin();
-	// std::advance(it, 2);
-	// std::swap(*vector.begin(), *vector.rbegin());
 
 	return (EXIT_SUCCESS);
 }
 
-void	merge(std::vector<int> &vec) {
-	std::size_t level = 0;
-	std::size_t vecSize = vec.size();
-	std::size_t step = std::pow(2, level);
-	std::size_t	groupSize = 2*step;
-	std::size_t	groupStart = step - 1;
-	std::size_t	numberOfGroups = vecSize / groupSize;
+// void	merge(std::vector<int> &vec00) {
+void	merge(int *input) {
+	// std::vector<int>	*main = &vec00;
+	// std::vector<int>	*pend = NULL;
+	// std::vector<int>	*tempVec = NULL;
 
-	while (numberOfGroups > 0) {
-		std::cout
-			<< " MERGE LEVEL: " << level << ENDL
-			<< " NB_OF_GROUPS: " << numberOfGroups << " TOTAL_VALID: " << (vecSize - numberOfGroups * groupSize) % step<< ENDL
-			<< " STEP: " << step << ENDL
-			<< " GROUP_START " << groupStart << ENDL
-			<< " GROUP_SIZE: " << groupSize << ENDL;
-		printMerge(level, vec);
+	int	*main = input;
+	int *pend = NULL;
+	int *tempVec = NULL;
 
-		for (std::size_t i = groupStart, g = 0 ; g < numberOfGroups ; i += groupSize, ++g) {
-			if (vec[i] > vec[i + step]) {
-				// std::cout << "TRUE: " << vec[i] << " | " << vec[i + step] << ENDL;
-				for (std::size_t j = i - (groupStart), c = 0; c < step; j++, c++) {
-					// std::cout << "IN FOR\n";
-					int	temp = vec[j];
-					vec[j] = vec[j + step];
-					vec[j + step] = temp;
-				}
-			}
-		}
-		printMerge(level, vec, false);
-		step = std::pow(2, ++level);
-		groupSize = 2*step;
-		groupStart = step - 1;
-		numberOfGroups = vecSize / groupSize;
-	}
+	ctrl	c;
+	c._vecSize = 26;
 
-	std::vector<int>	temp(26);
+	// c.up_order(0, *main);
+	c.up_order(0);
 
-	// groupSize /= 2;
-	// step /= 2;
-	// level /= 2;
+	while (c._nb_pairs > 0) {
+		print_status(&c);
+		// printMerge(c._level, *main, true, true);
+		printMerge(c._level, main, true, true);
 
-	step = std::pow(2, --level);
-	groupSize = 2*step;
-	groupStart = step - 1;
-	numberOfGroups = vecSize / groupSize;
-
-	std::cout
-		<< " > STEP: " << step << ENDL
-		<< " > GROUP_SIZE: " << groupSize << ENDL
-		<< " > FIRST_LEFT_OVER: " << vec[groupSize + 0] << ENDL;
-	for (std::size_t i = 0, left = 0; i < vecSize; ++i) {
-		if (i < groupSize && left < step && (vec[i] > vec[groupSize + left])) {
-			// std::cout << i << " TRUE\n";
-			temp[i] = vec[groupSize + left];
-			++left;
-		}
-		else if(i < groupSize + left)
-			temp[i] = vec[i - left];
-		else
-			temp[i] = vec[i];;
-	}
-	printMerge(level, temp);
-
-	step = std::pow(2, --level);
-	groupSize = 2*step;
-	groupStart = step - 1;
-	numberOfGroups = vecSize / groupSize;
-	printMerge(level, temp);
-
-
-
-
-
-};
-
-void	printMerge(std::size_t level, std::vector<int> &vec, bool before) {
-	std::size_t	step = std::pow(2, level);
-	std::size_t	groupSize = 2*step;
-	std::size_t	groupStart = step - 1;
-	std::size_t	nbOfGroups = vec.size() / groupSize;
-
-	std::size_t vecSize = vec.size();
-	// std::size_t	numberOfGroups = vecSize / groupSize;
-
-	std::size_t	newNB = nbOfGroups * groupSize;
-	std::size_t	XXX = vecSize - (vecSize - newNB) % step;
-
-	std::size_t	pos = 0;
-	bool		backGround = true;
-	if (before)
-		std::cout << BOLD COP "Merge iteration level: " << level << " | " << newNB << ENDL
-			<< vecSize - newNB << " | " << XXX << ENDL
-			<< "BEFORE: " RST;
-	else
-		std::cout << BOLD COP "AFTER:  " RST;
-
-	// std::size_t left = 0;
-	for (std::size_t i = 0, groupNb = 0; i <  vec.size(); ++i) {
-			if (i < XXX && groupNb >= nbOfGroups)
-				std::cout << U_LINE O_LINE;
-
-		// if (groupNb < newNB) {
-			// std::cout << RST;
-			if (i < XXX && backGround)
-				std::cout << BG_GS_0;
-			else if (i < XXX)
-				std::cout << BG_GS_15;
-			// else
-			// 	std::cout << RST;
-
-			// if (i <= groupStart) {
-			if (i < XXX && i <= groupStart) {
-				std::cout << YLW;
-				if (i == groupStart)
-					std::cout << H_YLW BOLD ITALIC;
-			}
-			// else if (i >= groupStart + 1) {
-			else if (i < XXX && i >= groupStart + 1) {
-				std::cout << D_BLU;
-				if (i == groupStart + step){
-					std::cout << V_BLU BOLD ITALIC;
-					groupStart += groupSize;
-				}
-			}
-		// }
-		// else {
-		// 	++left;
-		// 	if (left > step){
-		// 		std::cout << REVERSE BOLD;
+		// for (std::size_t i = c._pairStart, g = 0 ; g < c._nb_pairs ; i += c._pairSize, ++g) {
+		// 	if ((*main)[i] > (*main)[i + c._order]) {
+		// 		// std::cout << "TRUE: " << vec[i] << " | " << vec[i + order] << ENDL;
+		// 		for (std::size_t j = i - (c._pairStart), k = 0; k < c._order; j++, k++) {
+		// 			// std::cout << "IN FOR\n";
+		// 			int	temp = (*main)[j];
+		// 			(*main)[j] = (*main)[j + c._order];
+		// 			(*main)[j + c._order] = temp;
+		// 		}
 		// 	}
 		// }
 
+		for (std::size_t i = c._pairStart, g = 0 ; g < c._nb_pairs ; i += c._pairSize, ++g) {
+			if (main[i] > main[i + c._order]) {
+				// std::cout << "TRUE: " << vec[i] << " | " << vec[i + order] << ENDL;
+				for (std::size_t j = i - (c._pairStart), k = 0; k < c._order; j++, k++) {
+					// std::cout << "IN FOR\n";
+					int	temp = main[j];
+					main[j] = main[j + c._order];
+					main[j + c._order] = temp;
+				}
+			}
+		}
+
+		// printMerge(c._level, *main, false, true);
+		// c.up_order(++c._level, *main);
+		printMerge(c._level, main, false, true);
+		c.up_order(++c._level);
+
+	}
+
+	std::cout << BOLD COP ">>> START OF MERGE <<<" << RENDL;
+
+	// std::vector<int>	vec01(c._vecSize);
+	// pend = &vec01;
+	int	*vec01 = NULL;
+	pend = vec01;
+
+
+	// c.down_order(--c._level, *main);
+	c.down_order(--c._level);
+	print_status(&c);
+
+	// printMerge(c._level, *main, true, false);
+	printMerge(c._level, main, true, false);
+
+
+	if (c._nbElements_noLeftOver > c._nbElements_completePs) {
+		// for (std::size_t i = 1; i <= 2 * c._nb_pairs + 1; ++i) {
+		// 	std::cout
+		// 		<< " START COPY: " << (i - 1) * c._order << " AT: " << pend + (i * c._order) << ENDL
+		// 		<< " MAIN: " << i * c._pairStart << ENDL
+		// 		<< " CHECK: " << 2 * c._nb_pairs + 1 << ENDL
+		// 		<< " PEND: " << (2 * c._nb_pairs + 1) * c._pairStart << ENDL;
+		// 	if ((*main)[i * c._pairStart] > (*main)[(2 * c._nb_pairs + 1) * c._pairStart]) {
+		// 		std::cout << BOLD "IS TRUE" << RENDL;
+		// 		std::memcpy(pend + (i * c._order), main + (2 * c._nb_pairs + 1), c._order);
+		// 		break ;
+		// 	}
+		// }
+
+		for (std::size_t i = 1; i <= 2 * c._nb_pairs + 1; ++i) {
+			std::cout
+				<< " START COPY: " << (i - 1) * c._order << " AT: " << pend + (i * c._order) << ENDL
+				<< " MAIN: " << i * c._pairStart << ENDL
+				<< " CHECK: " << 2 * c._nb_pairs + 1 << ENDL
+				<< " PEND: " << (2 * c._nb_pairs + 1) * c._pairStart << ENDL;
+			if (main[i * c._pairStart] > main[(2 * c._nb_pairs + 1) * c._pairStart]) {
+				std::cout << BOLD "IS TRUE" << RENDL;
+				std::memcpy(pend + (i * c._order), main + (2 * c._nb_pairs + 1), c._order);
+				break ;
+			}
+		}
+
+		// printMerge(c._level, (*main), false, false);
+		// printMerge(c._level, (*pend), false, false);
+		printMerge(c._level, main, false, false);
+		printMerge(c._level, pend, false, false);
+
+		// for (std::size_t i = 0, left = 0; i < c._vecSize; ++i) {
+		// 	if (i < c._pairSize && left < c._order && ((*main)[i] > (*main)[c._pairSize + left])) {
+		// 		// std::cout << i << " TRUE\n";
+		// 		(*pend)[i] = (*main)[c._pairSize + left];
+		// 		++left;
+		// 	}
+		// 	else if(i < c._pairSize + left)
+		// 		(*pend)[i] = (*main)[i - left];
+		// 	else
+		// 		(*pend)[i] = (*main)[i];;
+		// }
+
+		tempVec = main;
+		main = pend;
+		pend = tempVec;
+	}
+	// printMerge(c._level, (*main), false, false);
+	printMerge(c._level, main, false, false);
+
+	// c.down_order(--c._level, *main);
+	// printMerge(c._level, (*main), true, false);
+};
+
+// void	printMerge(std::size_t level, std::vector<int> &vec, bool before, bool merge) {
+void	printMerge(std::size_t level, int *vec, bool before, bool merge) {
+	// std::size_t vecSize = vec.size();
+	std::size_t vecSize = 26;
+	std::size_t order = 1ull << level;
+	std::size_t	pairStart = order - 1;
+	std::size_t	pairSize = 2 * order;
+	std::size_t	nb_pairs = vecSize / pairSize;
+
+	std::size_t	nbElements_completePs = nb_pairs * pairSize;
+	std::size_t	leftOverElements = (vecSize - nbElements_completePs) % order;
+	std::size_t	nbElements_noLeftOver = vecSize - leftOverElements;
+
+	std::size_t	pos = 0;
+	bool		backGround = true;
+	if (before) {
+		if (merge)
+			std::cout << BOLD COP "Merge iteration level: " << level << ENDL
+				<< "BEFORE: " RST;
+		else
+			std::cout << BOLD COP "Insertion sort iteration level: " << level << ENDL
+				<< "BEFORE: " RST;
+	} else
+		std::cout << BOLD COP "AFTER:  " RST;
+
+	for (std::size_t i = 0, groupNb = 0; i <  vecSize; ++i) {
+		if (i < nbElements_noLeftOver) {
+			if (groupNb >= nb_pairs)
+				std::cout << U_LINE O_LINE;
+
+			if (backGround)
+				std::cout << BG_GS_0;
+			else if (i < nbElements_noLeftOver)
+				std::cout << BG_GS_15;
+
+			if (i <= pairStart) {
+				std::cout << YLW;
+				if (i == pairStart)
+					std::cout << H_YLW BOLD ITALIC;
+			} else if (i >= pairStart + 1) {
+				std::cout << D_BLU;
+				if (i == pairStart + order){
+					std::cout << V_BLU BOLD ITALIC;
+					pairStart += pairSize;
+				}
+			}
+		} else
+			std::cout << REVERSE BOLD;
+
 		std::cout << " " << vec[i] << " ";
-		if (++pos == groupSize) {
+		if (++pos == pairSize) {
 			backGround = !backGround;
 			pos = 0;
 			++groupNb;
