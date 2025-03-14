ed3ab94a57e57ba662084c5e9364765431da8556
onze-e-meia <tffuente@gmail.com>
2025-03-14 19:19:09 -0300

    2 more try

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -30,6 +30,7 @@ struct ctrl {
 	std::size_t _pairStart;
 	std::size_t _pairSize;
 	std::size_t _nb_pairs;
+	std::size_t	_nb_unpaired;
 	std::size_t _nbElements_completePs;
 	std::size_t _leftOverElements;
 	std::size_t _nbElements_noLeftOver;
@@ -41,6 +42,7 @@ struct ctrl {
 		_pairStart = _order - 1;
 		_pairSize = 2 * _order;
 		_nb_pairs = _vecSize / _pairSize;
+		_nb_unpaired = _vecSize % _pairSize;;
 		_nbElements_completePs = _nb_pairs * _pairSize;
 		_leftOverElements = (_vecSize - _nbElements_completePs) % _order;
 		_nbElements_noLeftOver = _vecSize - _leftOverElements;
@@ -53,8 +55,10 @@ struct ctrl {
 		_pairStart = _order - 1;
 		_pairSize = 2 * _order;
 		_nb_pairs = _vecSize / _pairSize;
+		_nb_unpaired = _vecSize % _pairSize;
 		_nbElements_completePs = _nb_pairs * _pairSize;
 		_leftOverElements = (_vecSize - _nbElements_completePs) % _order;
+		// _leftOverElements =  std::abs(static_cast<int>(_order) - static_cast<int>(_nb_unpaired));
 		_nbElements_noLeftOver = _vecSize - _leftOverElements;
 	}
 };
@@ -68,6 +72,7 @@ void	print_status(ctrl *c) {
 		<< " ORDER: " << c->_order << " PAIR_START: " << c->_pairStart << ENDL
 		<< " PAIR_SIZE: " << c->_pairSize << " NB_OF_PAIRS: " << c->_nb_pairs << ENDL
 		<< " NB_ELEMENTS_OF_COMPLETE_PAIRS: " << c->_nbElements_completePs << ENDL
+		<< " NB_UNPAIRED_ELEMENTS: " << c->_nb_unpaired << ENDL
 		<< " LEFTOVER_ELEMENTS: " << c->_leftOverElements % c->_order << ENDL
 		<< " NB_ELEMENTS_WITH_NO LEFTOVER :" << c->_nbElements_noLeftOver << ENDL;
 }
@@ -148,7 +153,111 @@ void	merge(int *input) {
 	printMerge(c._level, main, true, false);
 	isertion(c, &main, &pend);
 
-	// int	*tempArray = NULL;
+	c.down_order(--c._level);
+	print_status(&c);
+	printMerge(c._level, main, true, false);
+	isertion(c, &main, &pend);
+
+	c.down_order(--c._level);
+	print_status(&c);
+	printMerge(c._level, main, true, false);
+	isertion(c, &main, &pend);
+
+};
+
+void	isertion(ctrl c, int **m, int **p) {
+
+	int	*main = *m;
+	int	*pend = *p;
+
+	int	bi = 1;
+	int	ai = 1;
+
+	for (std::size_t i = 0; i < c._vecSize; ++i)
+		pend[i] = -1;
+
+	// START MAIN
+	std::memcpy(pend, main, c._pairSize * sizeof(int));
+	std::size_t	copied = 2 * c._order;
+	if (c._nb_pairs > 1) {
+		// std::cout << "IN MAIN\n";
+		std::memcpy(pend + 2 * c._order, main + 3 * c._order, c._order * sizeof(int));
+		copied += c._order;
+		++ai;
+	}
+
+	printMerge(c._level, pend, false, false);
+
+	for (std::size_t s = c._pairStart, i = 0; i < c._nb_pairs;  s += c._order, i++) {
+		// if (pend[s] > main[c._pairStart + 2 * (i + 1) * c._order]) {
+		if (pend[s] > main[c._pairStart + 2 * c._order]) {
+			std::cout
+				<< i << " | "
+				<< pend[s] << " | "
+				<< main[c._pairStart + 2 * c._order] << " | "
+				<< 2 * c._order << ENDL;
+			if (i < 2 * c._nb_pairs) {
+				std::size_t	to_move = copied - i * c._order;
+				memcpy(pend + ((i + 1) * c._order), pend + (i * c._order), to_move * sizeof(int));
+				std::cout << "TRUE\n";
+				printMerge(c._level, pend, false, false);
+			}
+			memcpy(pend + (i * c._order), main + (2 * c._order), c._order * sizeof(int));
+			copied += c._order;
+			++bi;
+
+			if (bi <= ai) {
+				// memcpy(pend + copied, main + ((2 * (i + 1) + 2) * c._order), c._order * sizeof(int));
+				memcpy(pend + copied, main + ((2 * (i + 1) + 2) * c._order), c._order * sizeof(int));
+				copied += c._order;
+				++ai;
+			}
+
+			memcpy(pend + copied, main + ((2 * (i + 1) + 3) * c._order), c._order * sizeof(int));
+			copied += c._order;
+			std::cout << " Copied: " << copied << ENDL;
+			printMerge(c._level, pend, false, false);
+			break ;
+		}
+	}
+
+	if (c._nbElements_noLeftOver > c._nbElements_completePs) {
+		// std::cout << "TRUE\n";
+		for (std::size_t s = c._pairStart, i = 0; i < 2 * c._nb_pairs;  s += c._order, i++) {
+			if (pend[s] > main[c._pairStart + 2 * c._order]) {
+				if (i < 2 * c._nb_pairs) {
+					std::size_t	to_move = copied - i * c._order;
+					memcpy(pend + ((i + 1) * c._order), pend + (i * c._order), to_move * sizeof(int));
+				}
+				memcpy(pend + (i * c._order), main + (2 * c._order), c._order * sizeof(int));
+				copied += c._order;
+			}
+		}
+	}
+
+	if (c._leftOverElements) {
+		memcpy(pend + copied, main + copied, c._leftOverElements * sizeof(int));
+	}
+
+
+	// if (c._nbElements_noLeftOver == c._nbElements_completePs) {
+	// 	std::cout << BOLD COP " >>>>> BUILD MAIN" RENDL;
+
+	// 	std::memcpy(pend, main, c._pairSize);
+	// 	std::memcpy(pend + 8, main + 12, c._order * sizeof(int));
+	// 	// size_t i = 2 + 1;
+	// 	// std::memcpy(pend + c._pairSize, main + i * c._order, c._order * sizeof(int));
+	// 	for (std::size_t i = 1; i < c._nb_pairs; ++i) {
+	// 		std::cout << i << " | " << i + 1 << " | " << 2 * i + 1<< ENDL;
+	// 		std::memcpy(pend + (i + 1) * c._order, main + (2 * i + 1) * c._order, c._order * sizeof(int));
+	// 	}
+
+	// 	// std::cout << BOLD COP " >>>>> BUILD PEND" RENDL;
+
+	// 	// for (std::size_t i = 1; i < c._nb_pairs; ++i) {
+	// 	// 	std::memcpy(pend + c._order, main + (2 + i) * c._order, c._order * sizeof(int));
+	// 	// }
+	// }
 
 	// if (c._nbElements_noLeftOver > c._nbElements_completePs) {
 	// 	for (std::size_t i = 1; i <= 2 * c._nb_pairs + 1; ++i) {
@@ -184,84 +293,6 @@ void	merge(int *input) {
 	// 			std::memcpy(pend + dest_index, main + dest_index, c._order * sizeof(int));
 	// 	}
 	// }
-	// tempArray = main;
-	// main = pend;
-	// pend = tempArray;
-	// printMerge(c._level, main, false, false);
-
-	// int	*tempArray = NULL;
-
-	// tempArray = main;
-	// main = pend;
-	// pend = tempArray;
-
-	c.down_order(--c._level);
-	print_status(&c);
-	printMerge(c._level, main, true, false);
-	isertion(c, &main, &pend);
-
-};
-
-void	isertion(ctrl c, int **m, int **p) {
-
-	int	*main = *m;
-	int	*pend = *p;
-
-	printMerge(c._level, pend, false, false);
-
-	if (c._nbElements_noLeftOver == c._nbElements_completePs) {
-		std::cout << BOLD COP " >>>>> BUILD MAIN" RENDL;
-
-		std::memcpy(pend, main, c._pairSize);
-		std::memcpy(pend + 8, main + 12, c._order * sizeof(int));
-		// size_t i = 2 + 1;
-		// std::memcpy(pend + c._pairSize, main + i * c._order, c._order * sizeof(int));
-		for (std::size_t i = 1; i < c._nb_pairs; ++i) {
-			std::cout << i << " | " << i + 1 << " | " << 2 * i + 1<< ENDL;
-			std::memcpy(pend + (i + 1) * c._order, main + (2 * i + 1) * c._order, c._order * sizeof(int));
-		}
-
-		// std::cout << BOLD COP " >>>>> BUILD PEND" RENDL;
-
-		// for (std::size_t i = 1; i < c._nb_pairs; ++i) {
-		// 	std::memcpy(pend + c._order, main + (2 + i) * c._order, c._order * sizeof(int));
-		// }
-	}
-
-	if (c._nbElements_noLeftOver > c._nbElements_completePs) {
-		for (std::size_t i = 1; i <= 2 * c._nb_pairs + 1; ++i) {
-			std::size_t	dest_index = (i - 1) * c._order;
-			std::size_t	src_index = (2 * c._nb_pairs) * c._order;
-			std::size_t	aValue = i * c._order - 1;
-			std::size_t	bValue = (2 * c._nb_pairs + 1) * c._order - 1;
-
-			std::cout
-				<< " DEST INDEX: " << dest_index << " AT: " << *(main + (dest_index)) << ENDL
-				<< " A_VALUE INEX: " << aValue << " VALUE: " << main[aValue] << ENDL
-				<< " CHECK: " << 2 * c._nb_pairs + 1 << ENDL
-				<< " SRC INDEX: " << src_index
-				<< " AT: " << *(main + src_index) << DENDL;
-
-			if (main[aValue] > main[bValue]) {
-				std::cout << BOLD "IS TRUE: " << main[aValue] << " | " << main[bValue] << RENDL;
-				std::memcpy(pend + dest_index, main + src_index, c._order * sizeof(int));
-
-				src_index = dest_index;
-				std::size_t	n = c._nbElements_noLeftOver - (i * c._order);
-				++i;
-				dest_index = (i - 1) * c._order;
-
-				// std::cout << c._nbElements_noLeftOver << " | " << i <<" REST: " << n << ENDL;
-
-				std::memcpy(pend + dest_index, main + src_index, n * sizeof(int));
-				n = c._vecSize - c._nbElements_noLeftOver;
-				std::memcpy(pend + c._nbElements_noLeftOver, main + c._nbElements_noLeftOver, n * sizeof(int));
-				break ;
-			}
-			else
-				std::memcpy(pend + dest_index, main + dest_index, c._order * sizeof(int));
-		}
-	}
 
 	int	*tempArray = *m;
 	*m = *p;
